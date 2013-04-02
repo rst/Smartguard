679d5f0a3368f28f6f250fc9ee8907e456f2bb22
Robert Thau <rst@smartleaf.com>
2013-04-01 22:14:25 -0400

    Move source files into proper place for gem-based plugin.

--- lib/smartguard.rb
+++ lib/smartguard.rb
@@ -1,2 +1,18 @@
-module Smartguard
-end
+require 'smartguard/version'
+require 'smartguard/access_test_helpers'
+require 'smartguard/access_form_helpers'
+require 'smartguard/access_require_privilege'
+require 'smartguard/access_sanity_tests'
+require 'smartguard/access'
+
+require 'smartguard/smartguard'
+require 'smartguard/smartguard_basic_role'
+require 'smartguard/smartguard_basic_permission'
+require 'smartguard/smartguard_basic_user'
+require 'smartguard/smartguard_basic_role_assignment'
+
+# Monkeypatch for Oracle adapter...
+
+require 'active_record/schema_dumper'
+require 'smartguard/smartguard_oracle_monkeypatch'
+
