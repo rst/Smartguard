765a6df97d43ae085baffe83d2e9f4497eb0b7b2
Robert Thau <rst@smartleaf.com>
2013-08-20 16:35:33 -0400

    KLUDGE! KLUDGE! KLUDGE! always set fixture_path, even if not clear we can.
    This shouldn't be failing, but it did.  Huh?  Ruby bug?

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,9 +10,7 @@ Rails.backtrace_cleaner.remove_silencers!
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-end
+ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 
 class ActiveSupport::TestCase
 
